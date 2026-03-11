980b910c94913797b0c9c1f33cf391f8212ea4f0
bhagavathts <bhagavathts@gmail.com>
2026-03-11 17:29:49 +0530

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
     render html:"hello"
   end
   include SessionsHelper
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,6 @@ module UsersHelper
     gravtar_id=Digest::MD5::hexdigest(user.email.downcase)
     size=options[:size]
     gravatar_url="http://gravatar.com/avatar/#{gravtar_id}?s=#{size}"
-    image_tag(gravatar_url, alt:user.name)
+    image_tag(gravatar_url, alt:user.name,class: "gravatar")
   end
 end
